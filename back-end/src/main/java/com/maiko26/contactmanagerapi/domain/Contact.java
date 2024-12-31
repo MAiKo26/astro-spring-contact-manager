4b3d3131713699512b6b1e2edbeb94739b33a29b
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-12-31 13:31:08 +0100

    Update Contact.java

--- back-end/src/main/java/com/maiko26/contactmanagerapi/domain/Contact.java
+++ back-end/src/main/java/com/maiko26/contactmanagerapi/domain/Contact.java
@@ -5,23 +5,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
-@Table(name="Contacts")
+@Table(name = "Contacts")
 public class Contact {
     @Id
     @UuidGenerator
-    @Column(name="id",unique=true,updatable=false)
+    @Column(name = "id", unique = true, updatable = false)
     private String id;
     private String name;
     private String email;
@@ -30,4 +22,82 @@ public class Contact {
     private String address;
     private String status;
     private String photoUrl;
+
+    public Contact(String id, String name, String email, String title, String phone, String address, String status, String photoUrl) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.title = title;
+        this.phone = phone;
+        this.address = address;
+        this.status = status;
+        this.photoUrl = photoUrl;
+    }
+
+    public Contact() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public String getPhotoUrl() {
+        return this.photoUrl;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
 }
