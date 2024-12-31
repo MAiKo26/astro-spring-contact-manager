627394cb6bd2e58b3c7aef70d08215fb3f6a0839
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-12-31 13:31:04 +0100

    Update ContactService.java

--- back-end/src/main/java/com/maiko26/contactmanagerapi/service/ContactService.java
+++ back-end/src/main/java/com/maiko26/contactmanagerapi/service/ContactService.java
@@ -3,8 +3,7 @@ package com.maiko26.contactmanagerapi.service;
 import com.maiko26.contactmanagerapi.domain.Contact;
 import com.maiko26.contactmanagerapi.repo.ContactRepo;
 import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -23,56 +22,59 @@ import static com.maiko26.contactmanagerapi.constant.Constant.PHOTO_DIRECTORY;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 @Service
-@Slf4j
 @Transactional(rollbackOn = Exception.class)
-@RequiredArgsConstructor
 public class ContactService {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(ContactService.class);
     private final ContactRepo contactRepo;
-    public Page<Contact> getAllContacts(int page, int size){
-        return contactRepo.findAll(PageRequest.of(page,size, Sort.by("name")));
+
+    public ContactService(ContactRepo contactRepo) {
+        this.contactRepo = contactRepo;
+    }
+
+    public Page<Contact> getAllContacts(int page, int size) {
+        return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
 
     }
 
-    public Contact getContact(String id){
+    public Contact getContact(String id) {
         return contactRepo.findById(id).orElseThrow(() -> new RuntimeException("Contact not found"));
     }
 
-    public Contact createContact(Contact contact){
+    public Contact createContact(Contact contact) {
         return contactRepo.save(contact);
     }
-    public void deleteContact(Contact contact){
-         contactRepo.delete(contact);
+
+    public void deleteContact(Contact contact) {
+        contactRepo.delete(contact);
     }
 
-    public String uploadPhoto(String id, MultipartFile file){
-        log.info("Saving picture for user ID: {}",id);
+    public String uploadPhoto(String id, MultipartFile file) {
+        log.info("Saving picture for user ID: {}", id);
         Contact contact = getContact(id);
-        String photoUrl = photoFunction.apply(id,file);
+        String photoUrl = photoFunction.apply(id, file);
         contact.setPhotoUrl(photoUrl);
         contactRepo.save(contact);
         return photoUrl;
 
     }
 
-    private final Function<String,String> fileExtension = filename -> Optional.of(filename).filter(name ->name.contains("."))
+    private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
             .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
 
-    private final BiFunction<String,MultipartFile,String> photoFunction = (id,image) -> {
-        String filename = id + fileExtension.apply(image.getOriginalFilename()) ;
-        try{
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String filename = id + fileExtension.apply(image.getOriginalFilename());
+        try {
             Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
-            if(!Files.exists(fileStorageLocation)){
+            if (!Files.exists(fileStorageLocation)) {
                 Files.createDirectories(fileStorageLocation);
             }
-            Files.copy(image.getInputStream(),fileStorageLocation.resolve(filename),REPLACE_EXISTING);
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(filename), REPLACE_EXISTING);
 
-            return ServletUriComponentsBuilder.fromCurrentContextPath().path("/contacts/image/"+ filename).toUriString();
-        }
-        catch (Exception e){
+            return ServletUriComponentsBuilder.fromCurrentContextPath().path("/contacts/image/" + filename).toUriString();
+        } catch (Exception e) {
             throw new RuntimeException("unable to save the image");
         }
     };
-
 
 
 }
